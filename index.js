2dbe3c769aa6f83215b8eb67344772c040b72663
dan-ss93 <dserrano93@protonmail.com>
2020-10-16 13:25:37 +0200

    Prueba de Github
    Tarea de prueba

---
+++ index.js
@@ -0,0 +1,18 @@
+//Probando objetos nativos en Javascript
+
+function listaPropiedades(){
+var texto=" ";
+var idioma=navigator.language;
+var url=location.href;
+var puerto=location.port;
+var titulo=document.title;
+var aleatorio=Math.floor(Math.random()*100+1);
+var fecha=new Date().getDay();
+var horaActual= new Date().getHours();
+var maximo=Math.max();
+var minimo=Math.min();
+
+
+
+
+}
