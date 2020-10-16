58d15ddd66bf62a034446481eda952af53807e28
dan-ss93 <dserrano93@protonmail.com>
2020-10-16 18:50:40 +0200

    Test

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
-//Probando objetos nativos en Javascript
+/*console.log("éste mensaje se muestra por consola");
 
-function listaPropiedades(){
-var texto=" ";
-var idioma=navigator.language;
-var url=location.href;
-var puerto=location.port;
-var titulo=document.title;
-var aleatorio=Math.floor(Math.random()*100+1);
-var fecha=new Date().getDay();
-var horaActual= new Date().getHours();
-var maximo=Math.max();
-var minimo=Math.min();
+var lista;
+
+window.onload = function listaPropiedades() {
+    
+    const idioma=navigator.language;
+    document.getElementById("idioma").innerHTML=lista;
+    const url=location.href;
+    document.getElementById("url").innerHTML=lista;
+    const puerto=location.port;
+    document.getElementById("puerto").innerHTML=lista;
+    const titulo=document.title;
+    document.getElementById("titulo").innerHTML=lista;
+    let aleatorio=Math.floor(Math.random()*100+1);
+    document.getElementById("random").innerHTML=lista;
+    let fecha=new Date().getDay();
+    document.getElementById("fecha").innerHTML=lista;
+    let horaActual=new Date().getHours();
+    document.getElementById("hora").innerHTML=lista;
+    const maximo=Math.max();
+    document.getElementById("max").innerHTML=lista;
+    const minimo=Math.min();
+    document.getElementById("min").innerHTML=lista;
+    var altura=screen.availHeight;
+    document.getElementById("altura").innerHTML=lista;
+ 
+    }
+    
+
+  
+
+    
 
 
 
 
-}
+
