1d3da6db7c2ece41c358c4bce2ce39661ea3f5ae
dan-ss93 <dserrano93@protonmail.com>
2020-10-18 15:20:08 +0200

    New changes

--- index.js
+++ index.js
@@ -20,92 +20,26 @@ function inicio(){
 //Ex. 3
 function input_titulo () {
     document.cookie = document.getElementById("input_titulo").value;
-    //alert(document.cookie); 
-    }
+    console.log(document.cookie); //Para corroborar la cookie
+}
 
 //Ex. 4
 
 window.onload = cuentaAtras;
-var segundos = 5;
+    var segundos = 5;
 
     function cuentaAtras() {
+
     document.getElementById('countdown').innerHTML = segundos;
 
     if(segundos==0){
-    window.open("numero.html", "", "height=300, width=300");
+    window.open("numero.html","","height=300,width=300,top=400,left=-1100","menubar=0");
 
-}else{
-segundos-=1;
-setTimeout("cuentaAtras()",1000);
-}
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*console.log("éste mensaje se muestra por consola");
-
-var lista;
-
-window.onload = function listaPropiedades() {
-    
-    const idioma=navigator.language;
-    document.getElementById("idioma").innerHTML=lista;
-    const url=location.href;
-    document.getElementById("url").innerHTML=lista;
-    const puerto=location.port;
-    document.getElementById("puerto").innerHTML=lista;
-    const titulo=document.title;
-    document.getElementById("titulo").innerHTML=lista;
-    let aleatorio=Math.floor(Math.random()*100+1);
-    document.getElementById("random").innerHTML=lista;
-    let fecha=new Date().getDay();
-    document.getElementById("fecha").innerHTML=lista;
-    let horaActual=new Date().getHours();
-    document.getElementById("hora").innerHTML=lista;
-    const maximo=Math.max();
-    document.getElementById("max").innerHTML=lista;
-    const minimo=Math.min();
-    document.getElementById("min").innerHTML=lista;
-    var altura=screen.availHeight;
-    document.getElementById("altura").innerHTML=lista;
- 
+    }else{
+    segundos-=1;
+    setTimeout("cuentaAtras()",1000);
     }
-    
+}
 
-  
 
-    
 
-*/
---
+++ numero.js
@@ -0,0 +1,11 @@
+//Ex. 5
+
+function aleatorio(){
+random1 = Math.floor(Math.random() *10);
+window.document.getElementById("resultado").innerHTML=random1;
+console.log(random1);
+}
+
+
+
+
