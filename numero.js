9ced8c9be1e93df5558f107e38e3972070d3d311
dan-ss93 <dserrano93@protonmail.com>
2020-10-18 18:14:59 +0200

    New changes

--- numero.js
+++ numero.js
@@ -6,6 +6,23 @@ window.document.getElementById("resultado").innerHTML=random1;
 console.log(random1);
 }
 
+//Ex. 6
 
+function aciertaNumero(){
+    numero =document.getElementById("acertar").value;
+    console.log(random1,numero);
+
+if (random1==numero){
+    window.document.getElementById("acierta").innerHTML="Has acertado.";
+    window.close;
+
+
+}else if (random1>numero){
+    window.document.getElementById("acierta").innerHTML="El número aleatorio es mayor.";
+}else if (random1<numero){
+    window.document.getElementById("acierta").innerHTML="El número aleatorio es menor.";
+}
+
+}
 
 
