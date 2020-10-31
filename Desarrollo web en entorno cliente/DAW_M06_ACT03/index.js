dc811bd795d59da514ce7210c4c0e642cde843d3
dan-ss93 <dserrano93@protonmail.com>
2020-10-31 18:45:18 +0100

    New code.

--- Desarrollo web en entorno cliente/DAW_M06_ACT03/index.js
+++ Desarrollo web en entorno cliente/DAW_M06_ACT03/index.js
@@ -1,21 +1,46 @@
-
-class SerieTV
-  {
-  constructor (titulo, tematica, actoresPrincipales)
-    {
-    this.titulo    = titulo;
-    this.tematica = tematica;
-    this.actores   = [...actoresPrincipales];
+class SerieTV {
+    constructor (titulo, tematica, actoresPrincipales){
+        var arrayActores = new Array();
+        this.titulo=titulo;
+        this.tematica=tematica;
+        this.actores=actoresPrincipales;
+        
+    
+        this.generaActorFavorito = function(){
+            var long = actoresPrincipales.length;
+            let calc = Math.floor(Math.random()*(long));
+           arrayActores = actoresPrincipales[calc];
+           console.log(arrayActores);
+        }
+        
     }
-  generaActorFavorito()
-    {
-    console.log( this.actores[ Math.floor(Math.random()*this.actores.length ) ]);
-    }
-  }
-var show01 = new SerieTV('Dexter',  'Drama', ['Michael C Hall','Jennifer Carpenter']);
-var show02 = new SerieTV('Samurai Gourmet',  'Cocina',  ['Naoto Takenaka','Tetsuji Tamayama', 'Honami Suzuki'] );
-var show03 = new SerieTV('Breaking Bad', 'Drama', ['Aaron Paul','Bryan Cranston', 'RJ Mitte', 'Anna Gunn']);
- 
+}
+var show01= new SerieTV('Dexter ',  'Drama ', ['Michael C Hall ' ,'Jennifer Carpenter']);
 show01.generaActorFavorito();
+
+var show02 = new SerieTV('Samurai Gourmet' ,  'Cocina' ,  ['Naoto Takenaka' ,'Tetsuji Tamayama' , 'Honami Suzuki '] );
 show02.generaActorFavorito();
+
+var show03 = new SerieTV ('Breaking Bad ', 'Drama ', ['Aaron Paul ','Bryan Cranston ', 'RJ Mitte ', 'Anna Gunn ']); 
 show03.generaActorFavorito();
+
+console.log("-------------------------");
+
+
+
+
+var total_series = new Array();
+total_series.push(show01);
+total_series.push(show02);
+total_series.push(show03);
+console.log(total_series);
+
+console.log("-------------------------");
+
+function muestraArray(){
+window.document.getElementById("resultado")
+.innerHTML=`<table><tr><td>`+total_series[0].titulo +total_series[0].tematica+ total_series[0].actores+total_series[0].actoresPrincipales+`</td></tr>
+<tr><td>`+total_series[1].titulo +total_series[1].tematica+ total_series[1].actores+total_series[1].actoresPrincipales+`</td></tr>
+<tr><td>`+total_series[2].titulo +total_series[2].tematica+ total_series[2].actores+total_series[2].actoresPrincipales+`</td></tr>
+    </table>`;
+}
