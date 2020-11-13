73ae4c52219421196a7b4091e4a42ebd79297abd
dan-ss93 <dserrano93@protonmail.com>
2020-11-13 12:06:21 +0100

    new function

--- Desarrollo web en entorno cliente/DAW_M06_ACT04/index.js
+++ Desarrollo web en entorno cliente/DAW_M06_ACT04/index.js
@@ -16,3 +16,17 @@ function addCompetidor(){
     document.body.appendChild(listaHijos);
 
 }
+
+function finalizaCompeticion(){
+    lis = document.body.getElementsByTagName("li");
+    let tamañoLis = (lis.lenght)-1;
+
+    try{
+        lis[0].style.backgroundColor = "green";
+        lis[1].style.backgroundColor = "blue";
+        lis[2].style.backgroundColor = "orange";
+        lis[tamañoLis].style.backgroundColor = "red";
+    }catch(error){
+        console.log(error);
+    }
+}
