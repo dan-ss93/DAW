ccb30ca24d504a8e88cbc1276eb30418a15a8cf5
dan-ss93 <dserrano93@protonmail.com>
2020-11-13 22:49:11 +0100

    Finished.

--- Desarrollo web en entorno cliente/DAW_M06_ACT04/index.js
+++ Desarrollo web en entorno cliente/DAW_M06_ACT04/index.js
@@ -1,3 +1,6 @@
+/*1	Programa que al clicar en "AÑADE PARTICIPANTE" se añada el nombre introducido en el input "Nombre" como un nuevo participante 
+con el color de texto igual al escrito en el segundo input "Color".
+*/
 var listaHijos ;
 var lis = document.body.getElementsByTagName("li");
 
@@ -5,21 +8,27 @@ function addCompetidor(){
     event.preventDefault();
     let nombreCompetidor = document.getElementById("nombre").value;
     let colorCompetidor = document.getElementById("color").value;
+    let listaParticipantes = document.getElementById("listaParticipantes");
 
     listaHijos = document.createElement("li");
 
     listaHijos.innerHTML = nombreCompetidor;
     listaHijos.style.color = colorCompetidor;
     
-    listaParticipantes = listaHijos.parentNode;
-
-    document.body.appendChild(listaHijos);
+    listaParticipantes.appendChild(listaHijos);
 
 }
 
+/*2	Programa que al clicar en "FINALIZA COMPETICION" el color de fondo del 
+primer participante se cambie a verde, el segundo a azul, el 
+tercero a naranja y el último a rojo. 
+Si solo hay un participante se ha de mostrar verde. Captura los errores.
+*/
 function finalizaCompeticion(){
-    lis = document.body.getElementsByTagName("li");
-    let tamañoLis = (lis.lenght)-1;
+    lis= document.body.getElementsByTagName("li");
+    let tamañoLis = (lis.length) - 1;
+
+    console.log(tamañoLis)
 
     try{
         lis[0].style.backgroundColor = "green";
@@ -30,3 +39,40 @@ function finalizaCompeticion(){
         console.log(error);
     }
 }
+/*3	Programa que al clicaren "BORRA PARTICIPANTE" se borre el LI de la lista de 
+participantes que sea al número de hijo igual al escrito en el 3r input "Posición".
+*/
+function borraParticipante(){
+    let lista = document.getElementById("listaParticipantes");
+
+    const posicio = document.getElementById("posicion").value - 1;
+
+    lista.removeChild(lista.children[posicio])
+}  
+/*4	Programa que al clicar en "MUEVE PARTICIPANTE" se mueva el LI de la lista de participantes que sea al número de
+ hijo igual al escrito en el  3r input "Posición" y se sitúe en la posición escrita en el 4o input "Posición Final".
+*/
+function mueveParticipante(){
+    let primeraPosicion = document.getElementById("posicion").value - 1;
+    let ultimaPosicion = document.getElementById("posicionfinal").value;
+    
+    let lista = document.getElementById("listaParticipantes");
+    let primePos = lista.children[primeraPosicion];
+    let ultimaPos = lista.children[ultimaPosicion];
+    
+    lista.insertBefore( primePos, ultimaPos);
+}
+/*5	Programa que al clicar en "MODIFICA PARTICIPANTE" se cambie el texto del LI de la lista que sea al número de hijo 
+igual al escrito en el 3r input “Posición” y se sustituya por el texto escrito en el 1r input "Nombre".
+*/
+function modificaParticipante(){
+    let lista = document.getElementById("listaParticipantes");
+
+    const posicio = document.getElementById("posicion").value - 1;
+
+    const nombre = document.getElementById("nombre").value
+
+    lista.children[posicio].innerHTML= nombre
+    
+}
+
