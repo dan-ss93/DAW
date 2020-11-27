de0ebb56680032da475663d27224f24f152d0edc
dan-ss93 <dserrano93@protonmail.com>
2020-11-27 10:47:36 +0100

    Edited html and added some JS code.

--- Desarrollo web en entorno cliente/DAW_M06_ACT05/index.js
+++ Desarrollo web en entorno cliente/DAW_M06_ACT05/index.js
@@ -1,16 +1,40 @@
-function guardarDatos(){
-    event.preventDefault();
-    var nombre, password1, password2, mail, dni, aficion;
-    nombre = document.getElementById("inputNick").value;
-    password1 = document.getElementById("inputPassword1").value;
-    password2 = document.getElementById("inputPassword2").value;
-    email = document.getElementById("inputEmail").value;
-    dni = document.getElementById("inputDNI").value;
+document.addEventListener("DOMContentLoaded", function() {
+	document.getElementById("form").addEventListener('submit', validarFormulario); 
+  });
+  
+  function validarFormulario(evento) {
+	evento.preventDefault();
+	var usuario = document.getElementById('usuario').value;
+	if(usuario.length == 0) {
+	  alert('No has escrito nada en el usuario');
+	  return;
+	}
+	if(usuario.length <2 || usuario.length >25){
+		alert('El usuario debe tener entre 2 y 25 caracteres')
+		return;
+	}
+		
+	var password1 = document.getElementById('password').value;
+	var password2 = document.getElementById('password2').value;
+	if (password1.length < 4 || password1.length > 9 ) {
+	  alert('La contraseña no es válida');
+	  return;
+	}
+	if (password1 !== password2){
+		alert('Las contraseñas no coinciden');
+		return;
+	}
+	this.submit();
+  }
 
-    if (nombre ===""){
-        alert("El campo nombre está vacío");
-        return false;
-    }
 
+  let listaHijos;
+	function añadirAficion(){
+  	
+	  let nuevAficion = document.getElementById("nuevAficion").value;
+	  let listaAficiones = document.getElementById("aficiones").value;
 
-}
+	  listaHijos = document.createElement("nuevaOption");
+	  listaHijos.innerHTML = nuevAficion;
+	  listaAficiones.appenChild(listaHijos);
+	}
