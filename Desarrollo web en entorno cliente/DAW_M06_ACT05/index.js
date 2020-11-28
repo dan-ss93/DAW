d91adadc8d82ab8c6c854d155f35ed22fd078e71
dan-ss93 <dserso93@gmail.com>
2020-11-29 00:34:29 +0100

    added some code

--- Desarrollo web en entorno cliente/DAW_M06_ACT05/index.js
+++ Desarrollo web en entorno cliente/DAW_M06_ACT05/index.js
@@ -12,118 +12,160 @@ var aficion = document.getElementById("inputAficion");
 
 var dni = document.getElementById("inputDNI");
 
+
+
 function comprobarCheck(valido = true){
 	event.preventDefault();
 
 	let dato = document.forms["form"]["aceptar_condiciones"];
 
-	let user = document.getElementById("verifiTrue");
+	let check = document.getElementById("resultado");
 	
 	if (dato.checked == true){
 
-		user.innerHTML = "Aceptaste las condiciones";
-		user.style.color = "green";
+		check.innerHTML = "Aceptaste las condiciones";
+		check.style.background = "green";
 	}
-	else{
+	if(dato.checked==false){
 
-		user.innerHTML ="No aceptaste las condiciones de uso";
-		user.style.color ="red";
+		check.innerHTML ="No aceptaste las condiciones de uso";
+		check.style.background ="red";
 		valido = false;
 	}
 	return valido;
 }
 
-function validaNick(valido = true){
-
-	if (usuario.length >2 || usuario.length <25){
-
-		userCorrecto = window.document.getElementById("usuario").innerHTML="El usuario es correcto".
-		userCorrecto.style.color="green";
+function validarNick(valido = true){
+	let correcto = true;
+	let formulario = document.forms["form"];
+	let dato = formulario["inputNick"].value;
+	let user = document.getElementById("usuario");
+	
+	if (dato.length > 2 || dato.length < 25){
+		user.innerHTML="Nick válido";
+		user.style.background="green";
 	}
-
-	else{ 
-
-		(usuario.length <2 || usuario.length >25)
-		window.document.getElementById("usuario").innerHTML="El usuario debe tener entre 2 y 25 letras";
-		valido = false;
-	}
-
-function validarPassword(valido = true){
-
-	if (password1.length < 4 || password1.length > 9 ) {
-
-		window.document.getElementById("password1").innerHTML="La contraseña debe tener entre 4 y 9 letras o numeros";
-	  	return;
-	}
-	if (password1 !== password2){
-
-		window.document.getElementById("password2").innerHTML="Las contraseñas no coinciden";
-		valido = false;
+	if (dato.length <2 || dato.length > 25){
+		user.innerHTML="El nick debe tener entre 2 y 25 carácteres";
+		user.style.background="red";
+		correcto = false;
 	}
 	return valido;
+}
+
+function validarPassword(){
+
+	let correcto = true;
+	let formulario = document.forms["form"];
+	let dato = formulario["inputPass1"].value;
+	let user = document.getElementById("password1");
+	let expresion = new RegExp ("[0-9][A-z]{1,5}");
+
+	if (expresion.test(dato)){
+		user.innerHTML="Password aceptada";
+		user.style.background ="green"
+	}
+	else{
+		user.innerHTML="Ha de tener de 4 a 9 carácteres, números o letras.";
+		user.style.background ="red";
+		correcto = false;
+	}
+	return correcto;
+}
+
+function compararPasswords(password1){
+	let correcto = true;
+	let formulario = document.forms["form"];
+	let password2 = formulario["inputPass2"].value;
+	let user = document.getElementById("password2");
+
+	if (password1==password2){
+		user.innerHTML="Las contraseñas coinciden.";
+		user.style.background="green";
+	}
+	else{
+		user.innerHTML="Las contraseñas no coinciden."
+		user.style.background="red";
+		correcto = false;
+	}
+	return correcto;
 }
 function validarEmail(valido = true) {
 
-  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/.test(valido)){
+	let correcto = true;
+	let formulario = document.forms["form"];
+	let dato = formulario["inputEmail"].value;
+	let user = document.getElementById("email");
+	let expresion = new RegExp ("^[A-z0-9]+@[A-z0-9]+.[A-za-z0-9]{2,3}$");
 
-   alert("La dirección de email " + valor + " es correcta.");
-  } else {
-
-   alert("La dirección de email es incorrecta.");
-  }
+	if (expresion.test(dato)){
+		user.innerHTML="Mail correcto.";
+		user.style.background="green";
+	}
+	else{
+		user.innerHTML="Formato de mail incorrecto."
+		user.style.background="red";
+		correcto = false;
+	}
+	return correcto;
 }
-
 function validarDNI(valido = true){
 
-  var numero
+	var formulario = document.forms["form"];
+	var dato = formulario["inputDNI"].value;
+	let user = document.getElementById("dni");
+	var patt1 = new RegExp("^[0-9]{8,8}[A-z]{1,1}$");
+	let correcto = patt1.test(dato);
 
-  var letr
 
-  var letra
+	if(correcto){
+	user.innerHTML="Formato correcto.";
+	user.style.background="green";
+	}
+	else{
+		user.innerHTML="Formato incorrecto.";
+		user.style.background="red";
+		validado = false;
+	}
 
-  var expresion_regular_dni
- 
-  expresion_regular_dni = /^\d{8}[a-zA-Z]$/;
- 
-  if(expresion_regular_dni.test (dni) == true){
+	return correcto;
 
-	 numero = dni.substr(0,dni.length-1);
-	 
-	 letr = dni.substr(dni.length-1,1);
-	 
-	 numero = numero % 23;
-	 
-	 letra='TRWAGMYFPDXBNJZSQVHLCKET';
-	 
-	 letra=letra.substring(numero,numero+1);
-	 
-    if (letra!=letr.toUpperCase()) {
-
-       alert('Dni erroneo, la letra del NIF no se corresponde');
-     }else{
-
-       alert('Dni correcto');
-     }
-  }else{
-
-     alert('Dni erroneo, formato no válido');
-   }
-}
 }
 
+/*
+function validarAficiones(){
+
+	let correcto = true;
+	let formulario = document.forms["form"];
+	aficiones = document.getElementById("aficiones").selectedIndex;
+	let user = document.getElementById("hobby").value;
+	if (aficiones == null || aficiones == 0){
+		user.innerHTML="Aficiones aceptadas";
+		user.style.background="green";
+	}
+	else{
+		user.innerHTML="No has escogido aficiones";
+		user.style.background="red";
+		correcto = false;
+	}
+	return correcto;
+}
+*/
 function validarFormulario(){
 
 	let correcto = true;
 
 	correcto = comprobarCheck(correcto);
 
-	correcto = validaNick(correcto);
+	correcto = validarNick(correcto);
 
 	correcto = validarPassword(correcto);
 
 	correcto = validarEmail(correcto);
 
 	correcto = validarDNI(correcto);
+
+	//correcto = validarAficiones(correcto)
 
 	return correcto;
 }
@@ -143,17 +185,18 @@ function añadirAficion(){
 
 function guardarDatos(){
 
-	document.cookie = form["inputNick"].id + "=" + form ["inputNick"].value;
 
-	document.cookie = form["inputPass1"].id + "=" + form ["inputPass1"].value;
+	document.cookie = form["inputNick"].id + "=" + form["inputNick"].value;
 
-	document.cookie = form["inputPass2"].id + "=" + form ["inputPass2"].value;
+	document.cookie = form["inputPass1"].id + "=" + form["inputPass1"].value;
 
-	document.cookie = form["inputEmail"].id + "=" + form ["inputEmail"].value;
+	document.cookie = form["inputPass2"].id + "=" + form["inputPass2"].value;
 
-	document.cookie = form["inputDNI"].id + "=" + form ["inputDNI"].value;
+	document.cookie = form["inputEmail"].id + "=" + form["inputEmail"].value;
 
-	document.cookie = form["aceptar_condiciones"].id + "=" + form ["aceptar_condiciones"].value;
+	document.cookie = form["inputDNI"].id + "=" + form["inputDNI"].value;
+
+	document.cookie = form["aceptar_condiciones"].id + "=" + form["aceptar_condiciones"].value;
 
 	event.preventDefault();
 
@@ -183,6 +226,8 @@ function recuperarDatos(){
 	validarEmail();
 
 	validarDNI();
+
+	validarFormulario()
 
 	event.preventDefault();
 
